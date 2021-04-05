4086225ba755037b2b1046b33bdf83ca6fc9cc11
venegasjosh <venegasjosh@gmail.com>
2021-04-05 16:56:17 -0700

    adjusted font size of main title third attempt

--- src/components/title.js
+++ src/components/title.js
@@ -4,13 +4,9 @@ import { Row, Col, Jumbotron } from "reactstrap";
 
 function Title() {
   return (
-    <Row>
-      <Col>
         <Jumbotron>
           <h1 className="titulo">Citrus Lover</h1>
         </Jumbotron>
-      </Col>
-    </Row>
   );
 }
 
