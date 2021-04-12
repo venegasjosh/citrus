7a52b8519a2b6054e642e24435790be749542875
venegasjosh <venegasjosh@gmail.com>
2021-04-11 19:10:28 -0700

    added loading animation plus lazy loading for links in router

---
+++ src/components/loading/loading.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { DiamonLoading } from "react-loadingg";
+const Loading = () => {
+  return (<DiamonLoading  />);
+};
+export default Loading;
--- src/components/main.js
+++ src/components/main.js
@@ -1,23 +1,30 @@
-import { React} from "react";
-import { Switch, Route } from "react-router-dom";
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Container } from "reactstrap";
-import Entry from "../pages/entry";
-import Bio from "../pages/bio/bio";
-import Contact from "../pages/contact/contact";
-import ScrollToTop from "../components/scroll/ScrollToTop.js";
-import { BrowserRouter as Router } from "react-router-dom";
+
+// import Entry from "../pages/entry";
+// import Bio from "../pages/bio/bio";
+// import Contact from "../pages/contact/contact";
+// import ScrollToTop from "../components/scroll/ScrollToTop.js";
+import Loading from "./loading/loading";
+const Entry = lazy(() => import("../pages/entry"));
+const Bio = lazy(() => import("../pages/bio/bio"));
+const Contact = lazy(() => import("../pages/contact/contact"));
+const ScrollToTop = lazy(() => import("../components/scroll/ScrollToTop.js"));
 
 const Main = () => {
   return (
     <Router>
-      <Container>
-        <ScrollToTop />
-        <Switch>
-          <Route exact path="/" component={Entry}></Route>
-          <Route exact path="/bio" component={Bio}></Route>
-          <Route exact path="/contact" component={Contact}></Route>
-        </Switch>
-      </Container>
+      <Suspense fallback={<Loading />}>
+        <Container>
+          <ScrollToTop />
+          <Switch>
+            <Route exact path="/" component={Entry} />
+            <Route exact path="/bio" component={Bio} />
+            <Route exact path="/contact" component={Contact} />
+          </Switch>
+        </Container>
+      </Suspense>
     </Router>
   );
 };
