e03c34564ae62906671d2643bd73f761effee1d6
venegasjosh <venegasjosh@gmail.com>
2021-04-22 01:26:17 -0700

    added rounded white border around video player and gallery // also added responsive view for diff media types and screen sizes

--- src/components/video/video.js
+++ src/components/video/video.js
@@ -1,27 +1,26 @@
 import React from "react";
-import ReactPlayer from 'react-player'
-import { Container, Row, Col } from "reactstrap";
+import ReactPlayer from "react-player/lazy";
+import "./video.css";
+import { Row, Col } from "reactstrap";
 function Video() {
   return (
-    <Container className="video">
       <Row>
         <Col>
-          <div className="embed-responsive embed-responsive-16by9">
-         
-            <ReactPlayer 
-            title="Brailleman"
-              width="560"
-              height="315"
-              url="https://www.youtube.com/embed/Y88fJdH9xag"
-              frameborder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen="allowfullscreen"
-
+          <div className="skate">
+            <div className="embed-responsive embed-responsive-16by9">
+              <ReactPlayer
+                title="Brailleman"
+                width="100%"
+                height="100%"
+                url="https://www.youtube.com/embed/Y88fJdH9xag"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen="allowfullscreen"
               />
-             </div>
+            </div>
+          </div>
         </Col>
       </Row>
-    </Container>
   );
 }
 
