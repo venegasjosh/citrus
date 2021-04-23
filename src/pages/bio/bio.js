a39fcdc92e1bf896f2e6766da0a00c69a0646004
venegasjosh <venegasjosh@gmail.com>
2021-04-22 21:26:35 -0700

    fixed white border on right bug when zooming out

--- src/pages/bio/bio.js
+++ src/pages/bio/bio.js
@@ -1,22 +1,20 @@
 import React, { Suspense, lazy } from "react";
-import { Container} from "reactstrap";
 import Loading from "../../components/loading/loading";
 import Titlemini from "../../components/minititle/minititle";
 import Midwaytitle from "../../components/midwaytitle/midwaytitle";
 import Mediabar from "../../components/mediabar/mediabar";
-import "./bio.css"
-const Gallery = lazy(() => import("../../components/gallery/gallery"))
+import "./bio.css";
+const Gallery = lazy(() => import("../../components/gallery/gallery"));
 
 const Bio = (props) => (
-   <div fluid className="sam"> 
-  <Suspense fallback={<Loading />}>
-    <Titlemini />
-    
-    <Gallery />
-    <Midwaytitle />
-    <Mediabar />
+  <div fluid className="sam">
+    <Suspense fallback={<Loading />}>
+      <Titlemini />
+      <Gallery />
+      <Midwaytitle />
+      <Mediabar />
     </Suspense>
-  </div> 
+  </div>
 );
 
 export default Bio;
